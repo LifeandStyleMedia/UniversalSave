5047d1439ce412597c23b2db2e9a373df9cc469d
Jason Jones <jasonjones3d@gmail.com>
2020-11-20 02:38:01 -0500

    Added Object Dictionary and List

---
+++ Runtime/Types/ObjectDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Ludiq;
+
+namespace Lasm.Bolt.UniversalSaver
+{
+    /// <summary>
+    /// An AOTDictuonary replacement that can be serialized and saved.
+    /// </summary>
+    [Serializable][Inspectable]
+    [IncludeInSettings(true)]
+    [RenamedFrom("Lasm.BoltExtensions.IO.ObjectDictionary")]
+    [RenamedFrom("Lasm.BoltExtensions.ObjectDictionary")]
+    [RenamedFrom("Lasm.UAlive.ObjectDictionary")]
+    public sealed class ObjectDictionary : Dictionary<object, object> { }
+}
+
